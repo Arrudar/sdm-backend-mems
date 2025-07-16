2a1de24410858279564dff8a858bd156cbc8b1b5
Pedro Rico <pedro.rico@kellogg.northwestern.edu>
2025-07-16 23:14:29 +0000

    Add config.py for NTAG 424 DNA configuration

--- config.dist.py
+++ config.dist.py
@@ -5,8 +5,9 @@ DERIVE_MODE = "standard"
 MASTER_KEY = binascii.unhexlify("757bf1693bca463bb529ee1771c1ea09")
 
 # for encrypted mirroring
+ENC_FILE_DATA_PARAM = "enc_file_data"
 ENC_PICC_DATA_PARAM = "picc_data"
-ENC_FILE_DATA_PARAM = "enc"
+
 
 # for plaintext mirroring
 UID_PARAM = "uid"
@@ -17,3 +18,4 @@ SDMMAC_PARAM = "cmac"
 
 # accept only SDM using LRP, disallow usage of AES
 REQUIRE_LRP = False
+
